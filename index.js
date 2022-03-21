c3986226bbe31e059aadd5db2d148cff54aba593
Raphael Carlos <sec.ti@angra.rj.gov.br>
2022-03-21 18:01:41 -0300

    v1.2.1.9 - Logout com 5 min de inatividade.

--- index.js
+++ index.js
@@ -40,8 +40,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
     secret: process.env.SECRET,
-    saveUninitialized: true,
+    saveUninitialized: false,
     resave: true,
+    rolling: true,
+    cookie: { maxAge: 300000 },
   })
 );
 
