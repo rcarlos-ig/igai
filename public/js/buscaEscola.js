53422a9ab8b5fbc325973c77c7f5b3ba025d3e40
Raphael Carlos <rcarlos.ig@gmail.com>
2022-04-04 18:37:06 -0300

    v1.7.9 - Fix para busca de unidades nas tabelas.

--- public/js/buscaEscola.js
+++ public/js/buscaEscola.js
@@ -1,6 +1,14 @@
 function buscaEscola() {
   // Declare variables
-  let input, filter, table, rows, cell, index, strValue;
+  let input,
+    filter,
+    table,
+    tableMobile,
+    rows,
+    rowsMobile,
+    cell,
+    index,
+    strValue;
 
   input = document.getElementById("busca");
   filter = input.value.toUpperCase();
@@ -9,7 +17,7 @@ function buscaEscola() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (index = 1; index < rows.length; index++) {
-    cell = rows[index].getElementsByTagName("td")[1];
+    cell = rows[index].getElementsByClassName("unidade")[0];
     if (cell) {
       strValue = cell.textContent || cell.innerText;
 
@@ -17,6 +25,25 @@ function buscaEscola() {
         rows[index].style.display = "";
       } else {
         rows[index].style.display = "none";
+      }
+    }
+  }
+
+  tableMobile = document.getElementById("escolasMobile");
+
+  if (tableMobile) {
+    rowsMobile = tableMobile.getElementsByTagName("tr");
+
+    for (index = 1; index < rowsMobile.length; index++) {
+      cell = rowsMobile[index].getElementsByClassName("unidade")[0];
+      if (cell) {
+        strValue = cell.textContent || cell.innerText;
+
+        if (strValue.toUpperCase().indexOf(filter) > -1) {
+          rowsMobile[index].style.display = "";
+        } else {
+          rowsMobile[index].style.display = "none";
+        }
       }
     }
   }
