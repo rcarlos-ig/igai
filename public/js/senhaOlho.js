3d896dca45deb5da288f610dbe0cb28946e75356
Raphael Carlos <sec.ti@angra.rj.gov.br>
2022-04-19 16:51:30 -0300

    v1.8.3 - Função de visualização de senha no login e pop-ups nos itens do menu flutuante.

---
+++ public/js/senhaOlho.js
@@ -0,0 +1,14 @@
+function senhaOlho() {
+  const passwordInput = document.getElementById("password");
+  const eyeIcon = document.getElementById("senha-olho");
+
+  if (passwordInput.value === "") return false;
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.add("senha-olho-riscado");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("senha-olho-riscado");
+  }
+}
