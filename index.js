00b7e7de86f4a3898d3b1d6304a44c720204b0f3
Raphael Carlos <sec.ti@angra.rj.gov.br>
2022-05-23 15:40:59 -0300

    v1.13.8 - Integração com "Sentry error tracing".

--- index.js
+++ index.js
@@ -9,12 +9,40 @@ const passport = require("passport");
 const { loginCheck } = require("./controllers/auth/passport");
 const compression = require("compression");
 const bodyParser = require("body-parser");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 // DotEnv config
 dotenv.config();
 
 // Initialize Express
 const app = express();
+
+// Sentry initialization
+Sentry.init({
+  dsn: "https://d8529091aa49452b934278a38d3b951d@o1259225.ingest.sentry.io/6433768",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  release: "sejin-igaie@" + process.env.npm_package_version,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // MongoDB Connection
 const database = process.env.MONGOLAB_URI;
@@ -78,6 +106,11 @@ app.use("/", require("./routes/routes"));
 // Server
 const PORT = process.env.PORT || 80;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+// Sentry test route
+app.get("/debug-sentry", function mainHandler(_req, _res) {
+  throw new Error("My first Sentry error!");
+});
 
 // 404 error handling
 app.use((req, res) => {
