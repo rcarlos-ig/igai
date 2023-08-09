336a6cb4da34d91fcd77af88b9063f8328bb0234
Raphael Carlos <sec.ti@angra.rj.gov.br>
2023-08-09 17:47:13 -0300

    v2.1.5 - GTag obsoleta removida.

--- index.js
+++ index.js
@@ -75,9 +75,9 @@ app.use("/igaie/", Router);
 
 // Set the static folder
 if (process.env.NODE_ENV === "production") {
-  app.use("/igaie/", express.static(__dirname + "/public", { maxAge: "30d" }));
+  app.use("/igaie", express.static(__dirname + "/public", { maxAge: "30d" }));
 } else {
-  app.use("/igaie/", express.static(__dirname + "/public"));
+  app.use("/igaie", express.static(__dirname + "/public"));
 }
 
 // Server
